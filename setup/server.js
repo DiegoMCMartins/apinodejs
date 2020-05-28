904690140bcbc43535a2ddd782e73db1f86b9f4c
DiegoMCMartins <diegomcastro2.8@gmail.com>
2020-05-28 18:07:05 -0300

    New modifications

--- setup/server.js
+++ setup/server.js
@@ -6,6 +6,6 @@ server.use(restify.plugins.queryParser({ mapParams: false, allowDots: true }))
 server.use(restify.plugins.bodyParser({ mapParams: false }))
 
 server.start = () => {
-  server.listen(process.env.SERVER_PORT || 3000, () => console.log('Started'))
+  server.listen(process.env.PORT || 3000, () => console.log('Started'))
   return server
 }
