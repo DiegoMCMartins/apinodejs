48003e272461b06136d52067f5fda0d188a7aa54
DiegoMCMartins <diegomcastro2.8@gmail.com>
2020-05-28 18:59:05 -0300

    New

--- setup/server.js
+++ setup/server.js
@@ -5,7 +5,9 @@ export const server = restify.createServer()
 server.use(restify.plugins.queryParser({ mapParams: false, allowDots: true }))
 server.use(restify.plugins.bodyParser({ mapParams: false }))
 
+const port = process.env.PORT || 3000;
+
 server.start = () => {
-  server.listen(process.env.PORT || 3000, () => console.log('Started'))
+  server.listen(port, () => console.log('Started'))
   return server
 }
